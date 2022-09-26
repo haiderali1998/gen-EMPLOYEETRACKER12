6c830d9336e7764817986f5fa5b75c9322fd40c7
haiderali9823-xSomaK <haiderali9823@outlook.com>
2022-09-26 01:31:48 +0100

    corrected seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,5 @@ INSERT INTO department (name) VALUES ("Sales"), ("Finance"), ("Engineer"), ("Leg
 
 INSERT INTO role (title, salary, department_id) VALUES ("Sales Lead", 12000, 1), ("Accountant", 15000, 2), ("Lead Engineer", 19000, 3), ("Lawyer", 20000, 4), ("Salesperson", 1000, 1), ("Account Manager", 25000, 2), ("Software Engineer", 30000, 3), ("Legal Team Lead", 50000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Haider", "Ali", 1, 11, ("John", "Smith", 2, 12), ("Bob", "Williams", 3, 13), ("Jane", "Gold", 4, 14);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Haider", "Ali", 1, 11), ("John", "Smith", 2, 12), ("Bob", "Williams", 3, 13), ("Jane", "Gold", 4, 14);
 
